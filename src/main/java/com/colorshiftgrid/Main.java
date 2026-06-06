d8478ac67771ac25d02351f5f919a46b50c194bf
Iulia <iuliaszentes@gmail.com>
2026-06-06 20:09:34 +0300

    enable window resize

--- src/main/java/com/colorshiftgrid/Main.java
+++ src/main/java/com/colorshiftgrid/Main.java
@@ -35,7 +35,7 @@ public class Main extends Application {
 
         primaryStage.setTitle("Color Shift Grid");
         primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
+        primaryStage.setResizable(true);
         primaryStage.show();
     }
 }
