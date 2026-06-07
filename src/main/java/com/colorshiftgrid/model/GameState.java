ef466e39e000bdf00880fa6035c9407b50400c33
Iulia <iuliaszentes@gmail.com>
2026-06-07 22:12:52 +0300

    minimal improvements

--- src/main/java/com/colorshiftgrid/model/GameState.java
+++ src/main/java/com/colorshiftgrid/model/GameState.java
@@ -1,26 +1,13 @@
 package com.colorshiftgrid.model;
 
-public class GameState {
-    private int[][] grid;
-    private int steps;
-
+public record GameState(int[][] grid, int steps) {
     public GameState(int[][] grid, int steps) {
         this.steps = steps;
         int rows = grid.length;
         int cols = grid[0].length;
         this.grid = new int[rows][cols];
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                this.grid[i][j] = grid[i][j];
-            }
+            System.arraycopy(grid[i], 0, this.grid[i], 0, cols);
         }
-    }
-
-    public int[][] getGrid() {
-        return grid;
-    }
-
-    public int getSteps() {
-        return steps;
     }
 }
